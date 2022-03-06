1bfcf48544cbeca8c11e194018452840aa9065f1
wukaixin <wukaixin@qq.com>
2022-03-06 10:54:53 +0800

    first commit

---
+++ src/main/java/com/wkx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wkx.git;
+
+/**
+ * @author shkstart
+ * @create 2022-03-06 10:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
